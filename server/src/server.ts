26399d4bfa80626395af126de602838e01381413
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-03-01 18:41:39 -0800

    rewrote server.ts. Unable to verify if it works yet.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,36 @@
 import express from 'express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
 import path from 'node:path';
 import db from './config/connection.js';
-import routes from './routes/index.js';
 
-const app = express();
+
+import { typeDefs, resolvers } from './schemas/index.js';
 const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+const startApolloServer = async () => {
+  await server.start();
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use('/graphql', expressMiddleware(server));
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
 });
+
+
+startApolloServer();
