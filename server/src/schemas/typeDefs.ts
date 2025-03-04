ffe0a631448fb356b4945467947311578b567193
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-03-04 13:25:58 -0800

    rewrote typeDefs. I haven't been able to test the functionality yet, but I did confirm that the code compiles.

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -1,24 +1,51 @@
 const typeDefs = `
   type User {
-    _id: String!
-    username: String!
-    email: String!
-    password: String!
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
     savedBooks: [Book]!
   }
 
+  input UserInput {
+    username: String!
+    email: String!
+    password: String!
+  }
+
   type Book {
-      authors: [String]!
-      description: String!
-      bookId: String!
-      image: String!
-      link: String!
-      title: String!
+    bookId: String!
+    authors: [String!]!
+    description: String!
+    title: String!
+    image: String!
+    link: String!
+  }
+
+  input BookInput {
+    bookId: String!
+    authors: [String!]!
+    description: String!
+    title: String!
+    image: String!
+    link: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
-      user(_id: String, username: String): User
-        
+    user(_id: String, username: String): User
+    me: User   
+  }
+
+  type Mutation{
+    login(email: String!, password: String!): Auth
+    addUser(input: UserInput!): Auth
+    saveBook(book: BookInput!): User
+    removeBook(bookId: String!): User
   }
 
 `;
