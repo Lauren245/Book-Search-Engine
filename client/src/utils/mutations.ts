83eb3661a48c262b3c0dab5f3c0bc6a1ed78fb96
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-03-05 13:17:32 -0800

    wrote client-side mutations and queries.

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -1,3 +1,51 @@
-// import { gql } from '@apollo/client';
+import { gql } from '@apollo/client';
 
-// export const LOGIN_USER
+export const LOGIN_USER = gql `
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql `
+    mutation addUser($input: UserInput!) {
+        addUser(input: $input) {
+            user {
+                username
+                _id
+            }
+            token
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql `
+    mutation saveBook($input: BookInput!) {
+        saveBook(input: $input) {
+            bookId
+            authors
+            description
+            title 
+            image
+            link
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql `
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            bookId
+            authors
+            description
+            title 
+            image
+            link
+        }
+    }
+`;
--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -1,19 +1,19 @@
 import { gql } from '@apollo/client';
 
-const QUERY_USER = gql `
-    query user($username: String!) {
-        user(username: $username){
+export const GET_ME = gql `
+    query me {
+        me {
+            _id
             username
             email
-            books [
-                {
-                    authors[]
-                    description
-                    image 
-                    link
-                    title
-                }
-            ]
+            books {
+                bookId
+                authors
+                description
+                title 
+                image
+                link
+            }
         }
     }
-`;
+`
