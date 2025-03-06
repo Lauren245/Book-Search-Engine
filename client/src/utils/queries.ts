d8e5f11dbf7a38a16255a18540ccff9abdb82bda
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-03-05 19:22:33 -0800

    fixed small error in property names.

--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -6,7 +6,7 @@ export const GET_ME = gql `
             _id
             username
             email
-            books {
+            savedBooks {
                 bookId
                 authors
                 description
