d0f2f5a19297c6d607d94899f041a869e70cfb14
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-03-05 18:27:01 -0800

    added Apollo provider to App.tsx.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,51 @@
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from '@apollo/client';
+
+import { setContext } from '@apollo/client/link/context';
+
+import Auth from './utils/auth';
+
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+
+  //calls function in auth utils that gets authentication token from localstorage (if it exists).
+  const token = Auth.getToken();
+
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+
+});
+
+//set up client to execute the authLink middleware before making a request to the GraphQL API.
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
