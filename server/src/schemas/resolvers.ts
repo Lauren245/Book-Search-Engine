b683544b3dced8d0247b62bfe0d03b5c16d08254
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-03-05 22:24:20 -0800

    added missing context to server.ts. Added debugging console.log to save book resolver.

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -94,6 +94,8 @@ const resolvers = {
 
         // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
         saveBook: async (_parent: any, { book }: SaveBookArgs, context: any) => {
+            console.log('context.user = ', JSON.stringify(context.user));
+
             if(context.user){
                 return await User.findOneAndUpdate(
                     { _id: context.user._id},
