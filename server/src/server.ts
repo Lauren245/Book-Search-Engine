b51f949a78ff113d13ee1f7634f8c1fc099d5021
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-03-06 17:09:11 -0800

    changed how files are served.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
-import path from 'path';
 
 import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
@@ -20,27 +19,26 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   
-    app.use('/graphql', expressMiddleware(server as any,
-      {
-        context: authenticateToken as any
-      }
-    ));
+  app.use('/graphql', expressMiddleware(server as any, {
+    context: authenticateToken as any,
+  }));
 
-  // if we're in production, serve client/dist as static assets
+  // Serve static assets in production
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static('client/dist')); // Serve the static files
 
+    // Ensure that React handles routing in the client-side
     app.get('*', (_req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile('index.html', { root: 'client/dist' });
     });
   }
-  
+
   db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
   });
-};
+}
 
 startApolloServer();
